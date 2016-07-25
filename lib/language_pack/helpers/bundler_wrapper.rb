4b914b2029c360e41ff2deba39ad345b218e94fd
John Hawthorn <john@stembolt.com>
2016-07-25 15:02:50 -0700

    test

--- lib/language_pack/helpers/bundler_wrapper.rb
+++ lib/language_pack/helpers/bundler_wrapper.rb
@@ -33,7 +33,8 @@ class LanguagePack::Helpers::BundlerWrapper
 
   def install
     fetch_bundler
-    $LOAD_PATH << @path
+    $LOAD_PATH.unshift @path.to_s
+    p $LOAD_PATH
     require "bundler"
     self
   end
